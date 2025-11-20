c76717a01a0386a2d290aeaafdf8a01a05d82751
Xantya24 <tanya.sding@gmail.com>
2025-11-20 20:57:56 +0800

    updated the script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-//make a function for bus timing
 async function getBusTiming() {
   const inputBox = document.getElementById('busStopId');
   const busStopId = inputBox.value; //18141
 
-  // get the link to sync it up
   const link = 'https://sg-bus-arrivals.vercel.app/?id=' + busStopId;
   const response = await fetch(link);
   const data = await response.json();
 
-  //get the data of the buses
   const buses = data.services;
 
-  let busHtml = '';
+  let busHTML = document.getElementById('arrival').tBodies[0];
+  busHTML.innerHTML = '';
+
   for (bus of buses) {
-    const div = `<th>Bus No: ${bus.bus_no}</th>
-                 <th>Operator: ${bus.operator}</th>
-                 <th>Next Arrival: ${bus.next_bus_mins} minutes</th>`;
-    busHtml += div;
-    console.log(busHtml);
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td>${bus.bus_no}</td>
+    <td>${bus.operator}</td>
+    <td>${bus.next_bus_mins} minutes</td>`;
+
+    busHTML.appendChild(row);
   }
   const resultDiv = document.getElementById('arrivalInfo');
   resultDiv.innerHTML = busHtml;
